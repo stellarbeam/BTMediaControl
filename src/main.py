476192d92dab52cbeebee9c8ef2811570df049e6
Kashinath Patekar <42115482+helix026@users.noreply.github.com>
2019-04-27 21:27:23 +0530

    Separate GUI into a module

--- src/main.py
+++ src/main.py
@@ -1,52 +1,85 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-from __future__ import print_function
-try:
-    from Tkinter import *
-except:
-    from tkinter import *
-
+from time import sleep
+from threading import Thread
+from gi.repository.GLib import MainLoop, timeout_add
+from gui import Window
 
 import pydbus
-from time import sleep
 
 class MediaPlayer:
     def __init__(self):
+
+        self.DefineFunctionArgList()
+
         self.service = pydbus.SystemBus().get('org.bluez', '/')
+        self.Update_obj_list()
+
+        self.player_connected = False
+        self.TerminateFlag = False
+
+        self.obj_list_updater_thread = Thread(target=self.ObjListUpdater)
+        self.track_info_updater_thread = Thread(target=self.TrackInfoUpdater)
+
+        self.obj_list_updater_thread.start()
+        self.track_info_updater_thread.start()
+
         self.Start()
 
+    def ObjListUpdater(self):
+        self.system_bus = pydbus.SystemBus()
+        self.system_bus.subscribe(sender='org.bluez',signal='PropertiesChanged',signal_fired=self.Update_obj_list)
+
+        loop = MainLoop()
+        timeout_add(100,self.checkTerminate,loop)
+        loop.run()
+
+    def Update_obj_list(self, *args):
+        self.obj_list = self.service.GetManagedObjects()
+
+    def TrackInfoUpdater(self): # To be used by gui
+        while self.TerminateFlag is False:
+            try:
+                self.pos = self.media_player.Get('org.bluez.MediaPlayer1','Position')
+                self.track_info = self.media_player.Get('org.bluez.MediaPlayer1','Track')
+            except:
+                self.pos = 0
+                self.track_info = {
+                    'Title'   :''  ,
+                    'Duration':0   ,
+                    'Album'   :''  ,
+                    'Artist'  :''  ,
+                    'Genre'   :''
+                }
+
+            sleep(0.2)
+
     def Start(self):
-        print('Connecting to Media Player...')
         self.ConnectMediaPlayer()
 
     def ConnectMediaPlayer(self):
-        player_connected = False
-        while player_connected is not True:
-            obj_list = self.service.GetManagedObjects()
-            for obj_path in obj_list.keys():
+        while self.player_connected is not True:
+            for obj_path in self.obj_list.keys():
                 if "player" in obj_path:
                     self.player_path = obj_path
-                    self.device_path = obj_list[self.player_path]['org.bluez.MediaPlayer1']['Device']
-                    self.device_name = obj_list[self.device_path]['org.bluez.Device1']['Alias']
-                    player_connected = obj_list[self.device_path]['org.bluez.MediaControl1']['Connected']
+                    self.device_path = self.obj_list[self.player_path]['org.bluez.MediaPlayer1']['Device']
+                    self.device_name = self.obj_list[self.device_path]['org.bluez.Device1']['Alias']
+                    self.player_connected = self.obj_list[self.device_path]['org.bluez.MediaControl1']['Connected']
                     break
             sleep(0.2)
 
         self.media_player = pydbus.SystemBus().get('org.bluez', self.player_path)
-        print('Media Player successfully Connected!     Device Name: ',end=' ')
-        print(self.device_name)
         self.CreateWindow()
 
     def CheckConnection(self):
-        obj_list = self.service.GetManagedObjects()
-        if self.player_path in obj_list and obj_list[self.device_path]['org.bluez.MediaControl1']['Connected'] is True:
-            self.root.after(200,self.CheckConnection)
-        else:
-            print('Media Player was disconnected!           Device Name: ',end=' ')
-            print(self.device_name)
-            print()
+        if self.player_path not in self.obj_list and \
+        self.obj_list[self.device_path]['org.bluez.MediaControl1']['Connected'] is False:
+            self.player_connected = False
             self.root.destroy()
             self.Start()
+
+    def isPlayerConnected(self):
+        return self.player_connected
 
     def Play(self,event):
         self.media_player.Play()
@@ -60,33 +93,29 @@ class MediaPlayer:
     def Previous(self,event):
         self.media_player.Previous()
 
+    def Terminate(self):
+        self.TerminateFlag = True
+
+    def checkTerminate(self,loop):
+        if self.TerminateFlag is True:
+            loop.quit()
+        else:
+            timeout_add(100,self.checkTerminate,loop)
+
+    def DefineFunctionArgList(self):
+        self.func_list = {
+            'Terminate':self.Terminate,
+            'Play':self.Play,
+            'Pause':self.Pause,
+            'Next':self.Next,
+            'Previous':self.Previous,
+            'isPlayerConnected':self.isPlayerConnected,
+            'CheckConnection':self.CheckConnection
+        }
+
     def CreateWindow(self):
-        self.root = Tk()
-        self.root.geometry('250x125')
-        self.root.title('BTMediaControl')
-        self.frame = Frame(self.root,width=500,height=200)
+        self.root = Window(self.func_list)
+        self.root.run()
 
-        #Buttons
-        self.PlayButton = Button(self.frame,text="Play")
-        self.PlayButton.bind('<Button-1>',self.Play)
-        self.PlayButton.pack(side=TOP,fill=X)
-
-        self.PauseButton = Button(self.frame,text="Pause")
-        self.PauseButton.bind('<Button-1>',self.Pause)
-        self.PauseButton.pack(side=TOP,fill=X)
-
-        self.NextButton = Button(self.frame,text="Next")
-        self.NextButton.bind('<Button-1>',self.Next)
-        self.NextButton.pack(side=TOP,fill=X)
-
-        self.PreviousButton = Button(self.frame,text="Previous")
-        self.PreviousButton.bind('<Button-1>',self.Previous)
-        self.PreviousButton.pack(side=TOP,fill=X)
-
-        self.root.resizable(0,0)
-        self.frame.pack()
-        self.root.after(200,self.CheckConnection)
-        self.root.mainloop()
-
-
-player = MediaPlayer()
+if __name__ == '__main__':
+    player = MediaPlayer()
